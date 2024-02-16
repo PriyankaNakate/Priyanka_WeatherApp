2cfd1747b53c07fc249fee626c6b09041d576e7d
PriyankaNakate <nakatepriyanka884@gmail.com>
2024-02-16 13:34:59 +0530

    correct the error

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 async function fetchWeatherData(cityName){
-    let url=`https://api.openweathermap.org/data/2.5/weather?q=tuljapur&appid=761588948b2ca457b07f2f7935550797`
+    let url=`https://api.openweathermap.org/data/2.5/weather?q=tuljapur&appid=761588948b2ca457b07f2f7935550797`;
     const response = await fetch(url)
     const data = await response.json();
     let city = document.getElementById("city");
     city.innerHTML = data.name;
-    let windspeed = document.getElementById("wind_speed");
-    windspeed.innerHTML=data.wind.speed+"km/hr";
+    let windSpeed = document.getElementById("wind_speed");
+    windSpeed.innerHTML = data.wind.speed + " km/hr";
     let humidity = document.getElementById("humidity");
-    humidity.innerHTML=data.main.humidity+" g/kg";
-    let temp=document.getElementById("temp");
-    temp.innerHTML=Math.round((data.main.temp)-273.15)+"°C";
+    humidity.innerHTML = data.main.humidity+" g/kg";
+    let temp = document.getElementById("temp");
+    temp.innerHTML = Math.round((data.main.temp)-273.15)+"°C";
     console.log(data);
 }
 
